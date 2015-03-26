9d19fdc4d637faa6ca28ff5316521c9d9f106ef7
Andrej Švec <andy.swec@gmail.com>
2015-03-26 01:12:46 +0100

    authorize with Spotify
    Spotify authorize url call and return code

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,7 +1,12 @@
 class LoginController < ApplicationController
   def login
+    url = URI.parse "https://accounts.spotify.com/authorize"
+    url.query = "client_id=#{CLIENT_ID}&response_type=code&redirect_uri=#{REDIRECT_URI}"
+    redirect_to url.to_s
   end
 
   def callback
+    code = params[:code]
+    puts "Code: #{code}"
   end
 end
