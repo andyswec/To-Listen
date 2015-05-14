7ae1d1bb1e4b58c932036684394879dbb6b03b26
Andrej Švec <andy.swec@gmail.com>
2015-05-15 00:50:50 +0200

    Refactor users displaying

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ require 'rspotify'
 
 class UsersController < ApplicationController
   def users
-    @users_sessions = UserSession.where(session_id: session[:session_id]).order(:created_at)
+    users_sessions = UserSession.where(session_id: session[:session_id]).order(:created_at)
+    @display_users = users_sessions.collect { |us| DisplayUser.new(us) }
   end
 end
